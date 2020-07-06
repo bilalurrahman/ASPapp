c342740b6d1508a3523f2025a9bb71d33f5e9237
Bilal Ur Rahman <bilal.ur.rahman@hotmail.com>
2020-07-06 13:51:00 +0300

    New files and more things

---
+++ SpiceProj/Areas/Admin/Controllers/MenuItemController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using SpiceProj.Data;
+using SpiceProj.Models.ViewModels;
+
+namespace SpiceProj.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class MenuItemController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _hostingEnvironment;
+        
+        [BindProperty]
+        public MenuItemViewModel MenuItemVM { get; set; }
+        public MenuItemController(ApplicationDbContext db, IWebHostEnvironment hostEnvironment)
+        {
+            _db = db;
+            _hostingEnvironment = hostEnvironment;
+            MenuItemVM = new MenuItemViewModel()
+            {
+                Category = _db.Category,
+                MenuItem = new Models.MenuItem()
+            };
+        }
+        public async Task<IActionResult> Index()
+        {
+            var menuItems = await _db.MenuItem.Include(m=>m.Category).Include(m=>m.SubCategory).ToListAsync();
+            return View(menuItems);
+        }
+
+        //Get-Create
+
+        public IActionResult Create()
+        {
+            return View(MenuItemVM);
+        }
+    }
+}
